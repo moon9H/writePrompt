edb7614c854de9c12d6ac91306d3470dec478539
thdgmlwns1 <xhrl233@naver.com>
2026-06-05 16:08:12 +0900

    feature : 리턴타입 변경

--- src/main/java/com/ssafy/wp/controller/MemberController.java
+++ src/main/java/com/ssafy/wp/controller/MemberController.java
@@ -4,13 +4,13 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,21 +25,23 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/members")
 public class MemberController {
 	
-	@Autowired
 	private final MemberService mService;
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<Member> select(@PathVariable("id") int id){
+	public ResponseEntity<?> select(@PathVariable("id") int id){
 		Member member = mService.select(id);
 		
 		if (member != null) {
-			return ResponseEntity.ok(member);
+			return ResponseEntity.ok(Map.of(
+                    "message", "회원 정보 수정 성공",
+                    "data", member));
 		} else {
-			return ResponseEntity.notFound().build();
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+			        .body(Map.of("message", "회원정보 찾을 수 없음"));
 		}
 	}
 	
-	@PostMapping
+	@PostMapping // 로그인 기능으로 이동할 예정
 	public ResponseEntity<Member> insert(@RequestBody Member member){
 		
 		int result = mService.insert(member);
@@ -72,14 +74,16 @@ public class MemberController {
 	}
 	
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> delete(@PathVariable("id") int id){
+	public ResponseEntity<?> delete(@PathVariable("id") int id){
 		
 		int result = mService.delete(id);
 		
 		if (result > 0) {
-			return ResponseEntity.noContent().build();
+			return ResponseEntity.status(HttpStatus.NO_CONTENT)
+					.body(Map.of("message", "탈퇴 성공"));
 		} else {
-			return ResponseEntity.badRequest().build();
+			return  ResponseEntity.badRequest()
+                    .body(Map.of("message", "잘못된 입력"));
 		}
 	}
 }
