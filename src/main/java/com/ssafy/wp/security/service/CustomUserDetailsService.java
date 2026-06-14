045ec32b93cb36839813dcc7b1cd798f01e15aab
thdgmlwns1 <xhrl233@naver.com>
2026-06-14 14:34:19 +0900

    CoustomUserDetaill 설정

---
+++ src/main/java/com/ssafy/wp/security/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.ssafy.wp.security.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.wp.model.dao.MemberDao;
+import com.ssafy.wp.model.dto.Member;
+import com.ssafy.wp.security.dto.CustomUserDetails;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberDao memberDao;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        Member member = memberDao.selectByEmail(email);
+
+        if (member == null) {
+            throw new UsernameNotFoundException("존재하지 않는 회원입니다.");
+        }
+
+        return new CustomUserDetails(member);
+    }
+}
